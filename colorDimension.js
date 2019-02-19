757576d56b2de20c5e895989eb4a81a868498051
afelHipHop <afelrh@hotmail.com>
2019-02-19 12:14:01 -0500

    Update colorDimension.js

--- colorDimension.js
+++ colorDimension.js
@@ -1,31 +1,31 @@
 let colorDimension = {
   "2": {
     size: 64,
-    color: "#F35956"
+    color: "#00FF00"
   },
   "4": {
     size: 64,
-    color: "#F35956"
+    color: "#0101DF"
   },
   "8": {
     size: 64,
-    color: "#49BB6C"
+    color: "#0000FF"
   },
   "16": {
     size: 64,
-    color: "#2494C1"
+    color: "#FFFF00"
   },
   "32": {
     size: 64,
-    color: "#9659A7"
+    color: "#FF00FF"
   },
   "64": {
     size: 64,
-    color: "#F1C500"
+    color: "#00FFFF"
   },
   "128": {
     size: 36,
-    color: "#FF5956"
+    color: "#FFBF00"
   },
   "256": {
     size: 36,
@@ -33,14 +33,14 @@ let colorDimension = {
   },
   "512": {
     size: 36,
-    color: "#00000"
+    color: "#FA5858"
   },
   "1024": {
-    size: 64,
+    size: 26,
     color: "#24FFC1"
   },
   "2048": {
-    size: 64,
+    size: 26,
     color: "#A659A9"
   },
 }
